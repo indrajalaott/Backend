9b149dc54c9330b8f427f0670f6440ba3cc7c96d
sreejesh rayirath <servers@indrajala.in>
2024-08-14 16:25:32 +0530

    Your commit message here

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const path = require('path');
-
+const cors = require('cors'); // Import the cors package
 
 const admin = require("./routes/adminRoutes");
-const user =require("./routes/userRoutes");
-const Payment=require("./routes/PaymentRoutes")
+const user = require("./routes/userRoutes");
+const Payment = require("./routes/PaymentRoutes");
 
+const db = require("./models/mongoose");
 
-const db= require("./models/mongoose");
-
-const app =express();
-
+const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Enable CORS for all origins
+app.use(cors({
+  origin: '*', // Allow requests from any origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed methods
+  credentials: true // Allow credentials if needed
+}));
+
 app.use(express.json());
-app.use("/api/admin",admin);
-app.use("/api/user",user);
-app.use("/api/Payments",Payment);
+app.use("/api/admin", admin);
+app.use("/api/user", user);
+app.use("/api/Payments", Payment);
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-
-app.get("/",(req,res)=>{
-    res.send("Hello ,Shortfilm Viewers ...")
+app.get("/", (req, res) => {
+    res.send("Hello, Shortfilm Viewers ...");
 });
-app.listen(port,()=>{
-    console.log(`Server  running on port ${port}`);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
