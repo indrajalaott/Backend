72d24c03c2e8551d0dafd03d89356758bd282a22
dinosircode <sreejeshrayirath@gmail.com>
2024-08-17 22:30:58 +0530

    Updated Payment

---
+++ controllers/paymentControll.js
@@ -0,0 +1,18 @@
+const { Carousels } = require("../models/Carousels");
+const { Movies } = require("../models/Movies");
+const jwt = require('jsonwebtoken');
+const {User} = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const checkout = async (req, res) => {
+
+
+
+};
+
+
+module.exports={
+  
+  checkout
+  
+}
--- routes/PaymentRoutes.js
+++ routes/PaymentRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
+const paymentControll = require('../controllers/paymentControll');
 const {userAuth}=require('../middleware/userAuth');
 
 
 
 // register and login routes
 
-router.post('/phonepe',userAuth,userController.register);                                 //Tested
+router.post('/phonepe',userAuth,paymentControll.checkout);                               
 
 
 module.exports = router
