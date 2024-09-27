a7b2abcba50028bf4a53abe0ef6517faa7d279bf
dinosircode <sreejeshrayirath@gmail.com>
2024-09-27 19:30:28 +0530

    Update

--- controllers/paymentControll.js
+++ controllers/paymentControll.js
@@ -639,27 +639,26 @@ const checkPay = async (req, res) => {
             await sendPlanUpdateMail(email, subscriptionType, expiryDate);
 
 
-            res.redirect('https://indrajala.in/Home');
-            // res.status(200).json({ 
-            //     success: true,
-            //     message: "Payment verified, user updated, and email sent successfully" 
-            // });
+           
+            res.status(200).json({ 
+                success: true,
+                message: "Payment verified, user updated, and email sent successfully" 
+            });
         } catch (error) {
             console.error("Error in payment verification:", error);
-            res.redirect('https://indrajala.in/Broke');
-
-            // res.status(500).json({ 
-            //     success: false,
-            //     message: "Internal server error during payment verification" 
-            // });
+          
+            res.status(500).json({ 
+                success: false,
+                message: "Internal server error during payment verification" 
+            });
         }
     } else {
-        res.redirect('https://indrajala.in/Broke');
+     
 
-        // res.status(400).json({ 
-        //     success: false,
-        //     message: "Payment verification failed" 
-        // });
+        res.status(400).json({ 
+            success: false,
+            message: "Payment verification failed" 
+        });
     }
 };
 
