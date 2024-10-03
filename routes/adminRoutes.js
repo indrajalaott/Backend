8b91276019cc5dcac01a65ee1c90b8e8b9488320
dinosircode <sreejeshrayirath@gmail.com>
2024-10-03 15:32:58 +0530

    Upda

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -122,7 +122,7 @@ router.post('/searchUserByPhone', adminAuth, adminController.searchUserByPhoneNo
 //Routes Related to Movie Management  - IV Done by SREEJESH
 router.get('/showallmovies', adminAuth, adminController.getAllMovies);
 router.post('/AddtoList', adminAuth, adminController.addToRecomendationList);
-router.get('/ViewTop5Movies',adminAuth,adminController.viewTopFiveMovies);
+router.get('/ViewTop5Movies',adminController.viewTopFiveMovies);
 
 
 module.exports = router;
